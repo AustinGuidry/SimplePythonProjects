07cada09b8311952797b68ab0acf6a5c08164ade
Austin Guidry <128058966+AustinGuidry@users.noreply.github.com>
2023-10-21 14:11:14 -0500

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -6,7 +6,7 @@ ip_list = ['8.8.8.8', '8.8.4.4', '192.168.1.1']
 #the function 
 
 def ping_ip(ip_address):
-    response = os.system(f'ping -c 1 {ip_address}')
+    response = os.system(f'ping -c 5 {ip_address}')
     if response == 0:
         print(f'{ip_address} is up.')
     else:
